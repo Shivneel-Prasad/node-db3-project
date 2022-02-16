17526dc42c031deacc31300b73762226ca76672c
Shivneel-Prasad <Shivam201020@gmail.com>
2022-02-16 09:06:33 -0800

    Initial Commit

--- api/server.js
+++ api/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(morgan('dev'))
 server.use('/api/schemes', SchemeRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Multi-Table Queries</h2>
+      <p>Welcome to Multi-Table Queries</p>
+    `);
+});
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'Not Found',
+    })
+})
 
 module.exports = server;
